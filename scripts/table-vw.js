8264e7d3c0c7b381a7cdcf5f6cc7df4e5d0dc8c8
AungHein <143636800+aunghein-dev@users.noreply.github.com>
2025-03-18 23:46:27 +0630

    table-vw.js

--- scripts/table-vw.js
+++ scripts/table-vw.js
@@ -9,9 +9,9 @@ async function fetchDataForLastTwoMonths() {
   let allDates = [];
   let today = new Date();
   
-  // Calculate the start date (4 months ago)
+  // Calculate the start date (2 months ago)
   let startDate = new Date();
-  startDate.setMonth(startDate.getMonth() - 4);
+  startDate.setMonth(startDate.getMonth() - 2);
   
   let currentDate = new Date(startDate);
   let todayStr = today.toISOString().split('T')[0];
@@ -237,13 +237,3 @@ async function runRenderProcesses() {
 }
 
 runRenderProcesses();
-
-
-
-
-
-
-
-
-
-
